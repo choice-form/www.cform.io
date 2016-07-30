8a1de93a9733050fd4e1725d616c6119b7fd4bdd
shengxx <janisesheng@cform.io>
2016-07-31 01:04:06 +0800

    examples 内容页面完成

---
+++ app/components/examples/examples-list.js
@@ -0,0 +1,44 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+ data:[
+   {
+     title: 'feature one',
+     description: 'Far far away, behind the word mountains, far from the countries Vokalia ',
+   },
+   {
+     title: 'feature one',
+     description: 'Far far away, behind the word mountains, far from the countries Vokalia ',
+   },
+   {
+     title: 'feature one',
+     description: 'Far far away, behind the word mountains, far from the countries Vokalia ',
+   },
+   {
+     title: 'feature one',
+     description: 'Far far away, behind the word mountains, far from the countries Vokalia ',
+   },
+   {
+     title: 'feature one',
+     description: 'Far far away, behind the word mountains, far from the countries Vokalia ',
+   },
+   {
+     title: 'feature one',
+     description: 'Far far away, behind the word mountains, far from the countries Vokalia ',
+   },
+   {
+     title: 'feature one',
+     description: 'Far far away, behind the word mountains, far from the countries Vokalia ',
+   },
+   {
+     title: 'feature one',
+     description: 'Far far away, behind the word mountains, far from the countries Vokalia ',
+   },
+   {
+     title: 'feature one',
+     description: 'Far far away, behind the word mountains, far from the countries Vokalia ',
+   },
+ ],
+
+  iconTitle:['0','1','2','4']
+});
